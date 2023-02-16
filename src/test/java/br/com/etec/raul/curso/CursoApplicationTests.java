c53d8eb094662af5d70a59be63480cfb41837579
rauldangio <jojofagqwrt@gmail.com>
2023-02-16 13:41:55 -0300

    first commit

---
+++ src/test/java/br/com/etec/raul/curso/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.etec.raul.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
