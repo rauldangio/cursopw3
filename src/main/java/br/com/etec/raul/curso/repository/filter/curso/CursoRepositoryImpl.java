83218e8d00937e86c83d05fca819ae37c7e8cc6c
rauldangio <jojofagqwrt@gmail.com>
2023-05-18 08:55:09 -0300

    funcao criarWhere terminada

--- src/main/java/br/com/etec/raul/curso/repository/filter/curso/CursoRepositoryImpl.java
+++ src/main/java/br/com/etec/raul/curso/repository/filter/curso/CursoRepositoryImpl.java
@@ -2,17 +2,20 @@ package br.com.etec.raul.curso.repository.filter.curso;
 
 import br.com.etec.raul.curso.model.Curso;
 import br.com.etec.raul.curso.repository.filter.CursoFilter;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class CursoRepositoryImpl implements CursoRepositoryQuery{
 
@@ -27,12 +30,23 @@ public class CursoRepositoryImpl implements CursoRepositoryQuery{
 
         Predicate[] predicate = criarWhere(cursoFilter, builder, root );
         criteria.where(predicate);
+        criteria.orderBy(builder.asc(root.get("nomecurso")));
+
+        TypedQuery<Curso> query = manager.createQuery(criteria);
+
         return null;
     }
 
     private Predicate[] criarWhere(CursoFilter cursoFilter, CriteriaBuilder builder, Root<Curso> root) {
         List<Predicate> predicates = new ArrayList<>();
 
-        if (StringUtils)
+        if (StringUtils.isEmpty(cursoFilter.getNomecurso()))
+        {
+            predicates.add(builder.like(builder.lower(root.get("nomecurso")),
+                    "%" + cursoFilter.getNomecurso().toLowerCase() + "%"
+                    ));
+        }
+
+        return predicates.toArray(new Predicate[predicates.size()]);
     }
 }
