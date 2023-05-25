2794b343cb944e60cd473209599bd59bf8e4456f
rauldangio <jojofagqwrt@gmail.com>
2023-05-25 08:52:24 -0300

    paginação de curso concluida

--- src/main/java/br/com/etec/raul/curso/controller/CursoController.java
+++ src/main/java/br/com/etec/raul/curso/controller/CursoController.java
@@ -3,7 +3,10 @@ package br.com.etec.raul.curso.controller;
 
 import br.com.etec.raul.curso.model.Curso;
 import br.com.etec.raul.curso.repository.CursoRepository;
+import br.com.etec.raul.curso.repository.filter.CursoFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +19,12 @@ public class CursoController {
 
     @Autowired
     public CursoRepository cursoRepository;
+
+    @GetMapping()
+    public Page<Curso> pesquisar(CursoFilter cursoFilter, Pageable pageable)
+    {
+        return cursoRepository.filtrar(cursoFilter, pageable);
+    }
 
     @GetMapping("/todos")
     public List<Curso> listarTodosCursos(){
--- src/main/java/br/com/etec/raul/curso/repository/filter/curso/CursoRepositoryImpl.java
+++ src/main/java/br/com/etec/raul/curso/repository/filter/curso/CursoRepositoryImpl.java
@@ -4,6 +4,7 @@ import br.com.etec.raul.curso.model.Curso;
 import br.com.etec.raul.curso.repository.filter.CursoFilter;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 
 import javax.persistence.EntityManager;
@@ -33,14 +34,37 @@ public class CursoRepositoryImpl implements CursoRepositoryQuery{
         criteria.orderBy(builder.asc(root.get("nomecurso")));
 
         TypedQuery<Curso> query = manager.createQuery(criteria);
-
-        return null;
+        adicionarRestricoesDePaginacao(query, pageable);
+        return new PageImpl<>(query.getResultList(), pageable, total(cursoFilter));
     }
+
+    private Long total(CursoFilter cursoFilter) {
+        CriteriaBuilder builder = manager.getCriteriaBuilder();
+        CriteriaQuery<Long> criteria = builder.createQuery(Long.class);
+        Root<Curso> root = criteria.from(Curso.class);
+
+        Predicate[] predicate = criarWhere(cursoFilter, builder, root );
+        criteria.where(predicate);
+        criteria.orderBy(builder.asc(root.get("nomecurso")));
+        criteria.select(builder.count(root));
+        return manager.createQuery(criteria).getSingleResult();
+    }
+
+    private void adicionarRestricoesDePaginacao(TypedQuery<Curso> query, Pageable pageable) {
+        int paginaatual = pageable.getPageNumber();
+        int totalRegistroPorPagina = pageable.getPageSize();
+        int primeiroRegistroDaPagina = paginaatual * totalRegistroPorPagina;
+
+        query.setFirstResult(primeiroRegistroDaPagina);
+        query.setMaxResults(totalRegistroPorPagina);
+    }
+
+
 
     private Predicate[] criarWhere(CursoFilter cursoFilter, CriteriaBuilder builder, Root<Curso> root) {
         List<Predicate> predicates = new ArrayList<>();
 
-        if (StringUtils.isEmpty(cursoFilter.getNomecurso()))
+        if (!StringUtils.isEmpty(cursoFilter.getNomecurso()))
         {
             predicates.add(builder.like(builder.lower(root.get("nomecurso")),
                     "%" + cursoFilter.getNomecurso().toLowerCase() + "%"
