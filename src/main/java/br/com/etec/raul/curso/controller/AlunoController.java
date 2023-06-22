589288d85bd32bc8c311f52b750e684006ab206d
rauldangio <jojofagqwrt@gmail.com>
2023-06-22 08:59:22 -0300

    filtragem de alunos

--- src/main/java/br/com/etec/raul/curso/controller/AlunoController.java
+++ src/main/java/br/com/etec/raul/curso/controller/AlunoController.java
@@ -2,7 +2,11 @@ package br.com.etec.raul.curso.controller;
 
 import br.com.etec.raul.curso.model.Aluno;
 import br.com.etec.raul.curso.repository.AlunoRepository;
+import br.com.etec.raul.curso.repository.filter.AlunoFilter;
+import br.com.etec.raul.curso.repository.projections.AlunoDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,4 +24,11 @@ public class AlunoController {
     public List<Aluno> listarTodosAlunos(){
         return alunoRepository.findAll();
     }
+
+    @GetMapping
+    public Page<AlunoDTO> pesquisaAluno(AlunoFilter alunoFilter, Pageable pageable)
+    {
+        return alunoRepository.filtrar(alunoFilter, pageable);
+    }
+
 }
--- src/main/java/br/com/etec/raul/curso/repository/filter/aluno/AlunoRepositoryImpl.java
+++ src/main/java/br/com/etec/raul/curso/repository/filter/aluno/AlunoRepositoryImpl.java
@@ -5,6 +5,7 @@ import br.com.etec.raul.curso.model.Curso;
 import br.com.etec.raul.curso.repository.filter.AlunoFilter;
 import br.com.etec.raul.curso.repository.filter.CursoFilter;
 import br.com.etec.raul.curso.repository.projections.AlunoDTO;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
@@ -16,6 +17,8 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AlunoRepositoryImpl implements AlunoRepositoryQuery{
     @PersistenceContext
@@ -43,9 +46,8 @@ public class AlunoRepositoryImpl implements AlunoRepositoryQuery{
         TypedQuery<AlunoDTO> query = manager.createQuery(criteria);
         adicionarRestricoesDePaginacao(query, pageable);
 
-        return new PageImpl<>(query.getResultList(), pageable, total(AlunoFilter);
+        return new PageImpl<>(query.getResultList(), pageable, total(alunoFilter));
 
-        return null;
     }
 
     private void adicionarRestricoesDePaginacao(TypedQuery<?> query, Pageable pageable) {
@@ -68,5 +70,58 @@ public class AlunoRepositoryImpl implements AlunoRepositoryQuery{
         criteria.orderBy(builder.asc(root.get("nomealuno")));
         criteria.select(builder.count(root));
         return manager.createQuery(criteria).getSingleResult();
+    }
+
+    private Predicate[] criarRestricoes(CriteriaBuilder builder, Root<Aluno> root, AlunoFilter alunoFilter) {
+        List<Predicate> predicates = new ArrayList<>();
+
+        if (!StringUtils.isEmpty(alunoFilter.getNomealuno()))
+        {
+            predicates.add(builder.like(builder.lower(root.get("nomealuno")),
+                    "%" + alunoFilter.getNomealuno().toLowerCase() + "%"
+            ));
+        }
+
+        if (!StringUtils.isEmpty(alunoFilter.getNomecidade()))
+        {
+            predicates.add(builder.like(builder.lower(root.get("cidade").get("nomecidade")),
+                    "%" + alunoFilter.getNomecidade().toLowerCase() + "%"
+            ));
+        }
+        if (!StringUtils.isEmpty(alunoFilter.getNomecurso()))
+        {
+            predicates.add(builder.like(builder.lower(root.get("curso").get("nomecurso")),
+                    "%" + alunoFilter.getNomecurso().toLowerCase() + "%"
+            ));
+        }
+        if (!StringUtils.isEmpty(alunoFilter.getNomecidade()))
+        {
+            predicates.add(builder.equal(builder.lower(root.get("cidade").get("uf")),
+                     alunoFilter.getUf().toLowerCase()
+            ));
+        }
+        /*
+        // datas maiores que a inserida
+        if (alunoFilter.getData() != null)
+        {
+            predicates.add(builder.greaterThanOrEqualTo(builder.lower(root.get("data")),
+                     alunoFilter.getNomecurso()
+            ));
+        }
+
+        // data menores que a sua
+        if (alunoFilter.getData() != null)
+        {
+            predicates.add(builder.lessThanOrEqualTo(builder.lower(root.get("data")),
+                     alunoFilter.getNomecurso()
+            ));
+        }
+        */
+
+
+
+
+        return predicates.toArray(new Predicate[predicates.size()]);
+
     }
 }
